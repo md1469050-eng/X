91fbc8a1e829c1061ba3e8314aa108a1d8e9a139
BELALBOTX666 <v30333051@gmail.com>
2026-06-06 10:32:24 +0600

    Add files via upload

--- includes/database/index.js
+++ includes/database/index.js
@@ -1,24 +1,13 @@
 "use strict";
-const { Sequelize } = require("sequelize");
-const path = require("path");
+const Sequelize = require("sequelize");
+const { resolve } = require("path");
 const { DATABASE } = global.config;
-
 const dialect = Object.keys(DATABASE)[0];
-const storage = path.resolve(process.cwd(), DATABASE[dialect].storage);
-
-const sequelize = new Sequelize({
-  dialect,
-  storage,
+const storage = resolve(process.cwd(), `includes/${DATABASE[dialect].storage}`);
+const sequelize = new Sequelize({ dialect, storage,
   pool: { max: 20, min: 0, acquire: 60000, idle: 20000 },
   retry: { match: [/SQLITE_BUSY/], name: "query", max: 20 },
-  logging: false,
-  transactionType: "IMMEDIATE",
-  define: {
-    underscored: false,
-    freezeTableName: true,
-    charset: "utf8",
-    timestamps: true,
-  },
+  logging: false, transactionType: "IMMEDIATE",
+  define: { underscored: false, freezeTableName: true, charset: "utf8", timestamps: true },
 });
-
 module.exports = { sequelize, Sequelize };
--- includes/database/model.js
+++ includes/database/model.js
@@ -1,7 +1,14 @@
 "use strict";
-module.exports = function ({ Sequelize, sequelize }) {
-  const Users = require("./models/users")({ Sequelize, sequelize });
-  const Threads = require("./models/threads")({ Sequelize, sequelize });
-  const Currencies = require("./models/currencies")({ Sequelize, sequelize });
-  return { Users, Threads, Currencies };
+module.exports = function (input) {
+  const Users      = require("./models/users")(input);
+  const Threads    = require("./models/threads")(input);
+  const Currencies = require("./models/currencies")(input);
+  Users.sync({ force: false });
+  Threads.sync({ force: false });
+  Currencies.sync({ force: false });
+  return {
+    use: (n) => ({ Users, Threads, Currencies }[n]),
+    Users, Threads, Currencies,
+    model: { Users, Threads, Currencies },
+  };
 };
